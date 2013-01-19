aa95f4632526432b170f8c775eb9957b6f67f2fa
Nick Porillo <milkywayz@mail.com>
2013-01-18 21:56:09 -0500

    Cache content

--- src/net/minewriter/Book.java
+++ src/net/minewriter/Book.java
@@ -7,18 +7,26 @@ public class Book {
 
 	private JSONObject obj;
 	private boolean inv;
+	private String content;
 
 	public Book(JSONObject obj) {
 		this.obj = obj;
 		this.inv = false;
+		String cont = null;
+		try {
+			cont = obj.getString("Content");
+		} catch(Exception ex) {			
+		} finally {
+			content = cont;
+		}
 	}
 	
 	public Book() {
 		this.inv = true;
 	}
-	
-	public JSONObject getJSON() {
-		return this.obj;
+
+	public String getContent() {
+		return content;
 	}
 	
 	public String getTitle() throws JSONException {
