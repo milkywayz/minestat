389c13e6f4d670139c0f20e7b4b59568456c917d
Nick Porillo <milkywayz@mail.com>
2013-03-05 13:10:02 -0500

    ANSI Color nodes

---
+++ src/net/minewriter/Color.java
@@ -0,0 +1,13 @@
+package net.minewriter;
+
+public class Color {
+
+	private final static String P = "\u001B";
+	public final static String RESET = P + "[0m";
+	public final static String RED = P + "[31m";
+	public final static String GREEN = P + "[32m";
+	public final static String YELLOW = P + "[33m";
+	public final static String BLUE = P + "[34m";
+	public final static String MAGENTA = P + "[35m";
+	public final static String CYAN = P + "[36m";
+}
